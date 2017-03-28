692799ce171ff09e5b5f467b92998c8fcfa944a7
Tom <thauer@edu.aau.at>
2017-03-28 15:31:24 +0200

    Test-Push; Project- & libgdx-Setup.

---
+++ android/src/com/mygdx/malefiz/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.malefiz;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.malefiz.MyMalefizGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyMalefizGame(), config);
+	}
+}
