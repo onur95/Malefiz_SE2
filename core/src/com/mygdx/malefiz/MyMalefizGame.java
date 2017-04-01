bad7bca501f8087211394cc280b8b3d9e124a56e
onur95 <onur95@gmx.at>
2017-04-01 16:28:03 +0200

    Viewport auf FitViewport geändert. Einen Kreis hinzugefügt der sich nach jedem Klick an die entsprechende Position bewegt.

--- core/src/com/mygdx/malefiz/MyMalefizGame.java
+++ core/src/com/mygdx/malefiz/MyMalefizGame.java
@@ -2,34 +2,70 @@ package com.mygdx.malefiz;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
+
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
 
 public class MyMalefizGame extends ApplicationAdapter {
 	private Stage stage;
-	Texture txt;
-	Image img;
+	Texture txt_playground;
+	Texture txt_circle;
+	Image img_playground;
+	Image img_circle;
 
 	@Override
 	public void create () {
-		stage=new Stage(new StretchViewport(1500,1498));
+		stage=new Stage(new FitViewport(1500,1498));
 		Gdx.input.setInputProcessor(stage);
-		txt = new Texture("Playboard.jpg");
-		img=new Image(txt);
+		txt_playground = new Texture("Playboard.jpg");
+		txt_circle=new Texture("circle-xl.png");
+		img_playground=new Image(txt_playground);
+		img_circle=new Image(txt_circle);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.addActor(img);
+		img_playground.addListener(new ClickListener(){
+			public boolean touchDown(InputEvent event,
+									 float x,
+									 float y,
+									 int pointer,
+									 int button){
+				return true;
+			}
+			public void touchUp(InputEvent event,
+								float x,
+								float y,
+								int pointer,
+								int button){
+				MoveToAction action = new MoveToAction();
+				action.setPosition(x,y);
+				action.setDuration(0.2f);
+				img_circle.addAction(action);
+			}
+		});
+		stage.addActor(img_playground);
+		stage.addActor(img_circle);
 		stage.act();
 		stage.draw();
+	}
+
+	@Override
+	public void resize (int width, int height) {
+		// See below for what true means.
+		stage.getViewport().update(width, height, true);
 	}
 
 	@Override
