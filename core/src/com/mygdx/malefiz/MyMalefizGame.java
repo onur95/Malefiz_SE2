6b7e3823f3748f07a8ba971e3fb7397ca140368f
onur95 <onur95@gmx.at>
2017-04-01 12:07:11 +0200

    Bild auf Devicegröße skaliert

--- core/src/com/mygdx/malefiz/MyMalefizGame.java
+++ core/src/com/mygdx/malefiz/MyMalefizGame.java
@@ -4,31 +4,36 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class MyMalefizGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	// Yolo
-	
+	private Stage stage;
+	Texture txt;
+	Image img;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("Playboard.jpg");
+		stage=new Stage(new StretchViewport(1500,1498));
+		Gdx.input.setInputProcessor(stage);
+		txt = new Texture("Playboard.jpg");
+		img=new Image(txt);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.addActor(img);
+		stage.act();
+		stage.draw();
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		stage.dispose();
 	}
 }
