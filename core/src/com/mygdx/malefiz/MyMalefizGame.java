161f5656dc52c0676cd235887ac1e84af26589a4
Tom <thauer@edu.aau.at>
2017-04-09 19:02:33 +0200

    Prototype of Client-Server-Network within MyMalefizGame. Needs alot of Troubleshooting - Networking in Android is harder than expected. >> Important Changes *) Permission-Changes in manifest.xml *) Serverside-Tasks :: Alot Todo *) Clientside-Tasks :: Alot Todo >> Notes: To see for yourself, Server&Clientside-Tasks are currently commented. Watch out for START/END OF COMMENT. But be careful, here be dragons. It's probably a good idea to get another API for Networking services (Google API). Unless you want to hate Android & Networking forever, keep your hands off that src.

--- core/src/com/mygdx/malefiz/MyMalefizGame.java
+++ core/src/com/mygdx/malefiz/MyMalefizGame.java
@@ -4,17 +4,28 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.net.ServerSocket;
+import com.badlogic.gdx.net.Socket;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
+import com.badlogic.gdx.utils.async.AsyncTask;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+import javax.swing.text.View;
 
 
 public class MyMalefizGame extends ApplicationAdapter {
@@ -22,12 +33,37 @@ public class MyMalefizGame extends ApplicationAdapter {
 	Texture txt_playground;
 	Image img_playground;
 
+	// Networkingcomponents - Server
+	// Unless fit into 1 class, we might have a problem with multiple
+	// instances of these. Probable cleanup at a later time.
+	ServerSocket serverSocket;
+	static final int SocketServerPORT = 7331;	// {2000 .. 9999}
+	private Socket hostThreadSocket;
+	String ipAddress = "";
+	// Networkingcomponents - Client
+	Socket socket = null;
+	String destinationAddress;
+	int destinationPort;
+	// We need effective tradeoff between Server and Client-Components.
+
 	@Override
 	public void create () {
 		stage=new Stage(new FitViewport(1500,1498));
 		Gdx.input.setInputProcessor(stage);
 		txt_playground = new Texture("Playboard.jpg");
 		img_playground=new Image(txt_playground);
+
+		// Todo: For better performance, try to divide Server & Clientside properly
+		// -- Client does not need to run own server but to connect to one.
+		//Serverside:
+//		Thread hostSocketThread = new Thread(new SocketServerThread());
+//		hostSocketThread.start();
+		//Clientside:
+		// Impl.: Button to connect ::   b_Connect = (Button)findViewById(R.id.connect);
+		// Then:
+		//b_Connect.setOnClickListener(buttonConnectOnClickListener);
+
+
 		Board.init();
 		BoardToPlayboard.init();
 		Gdx.graphics.setContinuousRendering(false);
@@ -35,6 +71,7 @@ public class MyMalefizGame extends ApplicationAdapter {
 		stage.addActor(img_playground);
 		BoardToPlayboard.generate();
 	}
+
 
 	@Override
 	public void render () {
@@ -57,5 +94,173 @@ public class MyMalefizGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		stage.dispose();
+
+/*		// TODO :: Close ServerSocket accordingly
+		if (serverSocket != null) {
+			try {
+				serverSocket.close();  // Does not work for some reason
+			} catch (IOException e) {System.out.println("Error in onDestroy() :: "); e.printStackTrace();}
+		}
+*/	}
+
+
+/* START OF COMMENT :: NETWORKING
+
+	// TODO :: Serverside-cleanup
+	private class SocketServerThread extends Thread {
+
+		@Override
+		public void run() {
+			try {
+				serverSocket = new ServerSocket(SocketServerPORT);
+				MyMalefizGame.this.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						// TODO :: Upon start, do ...
+					}
+				});
+
+				while (true) {                                  // Set later to "While not playing game" >> Deactivate permanent search for clients
+					Socket socket = serverSocket.accept();
+
+					MyMalefizGame.this.runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							// TODO :: While running, update the game
+						}
+					});
+					SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(socket, count);
+					socketServerReplyThread.run();
+
+				}
+			} catch (IOException e) {System.out.println("Error in run() :: ");e.printStackTrace();}
+		}
+
 	}
+
+	private class SocketServerReplyThread extends Thread {
+		SocketServerReplyThread(Socket socket) {
+			hostThreadSocket = socket;
+			// TODO :: Send Data of gamefield
+			// C :: Stream in run() below
+		}
+
+		@Override
+		public void run() {
+			OutputStream outputStream;
+
+			try {   // Reply String msgReply to client accordingly via Stream
+				outputStream = hostThreadSocket.getOutputStream();
+				PrintStream printStream = new PrintStream(outputStream);
+				printStream.print(gamefield);	// TODO :: Correction of this
+				printStream.close();
+
+				MyMalefizGame.this.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						// TODO: gamefield.setGamefield(new gamefield)
+					}
+				});
+
+			} catch (IOException e) {e.printStackTrace();}
+
+			MyMalefizGame.this.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					// TODO: Pass turn to next player
+				}
+			});
+		}
+
+	}
+
+	private String getIpAddress() {
+		String ipAddress = "Lokal";
+
+		// This is common in most tutorials, thus implemented that way aswell.
+		try {
+			Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface.getNetworkInterfaces();
+			Enumeration<InetAddress> enumInetAddress = null;
+			// <=> Set enum for all acceptable Networkinterfaces ..
+			while (enumNetworkInterfaces.hasMoreElements()) {   // ... as long as there are more NetworkInterfaces
+				NetworkInterface networkInterface = enumNetworkInterfaces.nextElement();
+				enumInetAddress = networkInterface.getInetAddresses();
+				while (enumInetAddress.hasMoreElements()) { // .. as long as there are more passable adresses.
+					InetAddress inetAddress = enumInetAddress.nextElement();
+					if (inetAddress.isSiteLocalAddress()) { // If the add has that local address,
+
+						ipAddress = inetAddress.getHostAddress() + "\n"; // Give us the remote Host-IP
+
+                        //InetAddress addy = socket.getInetAddress();
+                        //String remoteIp = addy.getHostAddress();
+
+						// DONT USE STRINGBUILDER; looks damn glued
+					}
+				}
+			}
+		} catch (SocketException e) {e.printStackTrace();}
+		return ipAddress;
+	}
+
+
+	// TODO: Clientside-cleanup
+	OnClickListener buttonConnectOnClickListener =
+			new OnClickListener(){
+				@Override
+				public void onClick(View arg0) {
+					MyClientTask myClientTask = new MyClientTask(
+							et_Address.getText().toString(),
+							Integer.parseInt(ed_Port.getText().toString()));
+
+					myClientTask.execute();
+				}};
+
+	// Subclass :: In order to control scripting & usage later on. Purpose : Fetch data
+	public class MyClientTask extends AsyncTask<Void, Void, Void> {
+		MyClientTask(String designated_Adress, int designated_port){
+			destinationAddress = designated_Adress;
+			destinationPort = designated_port;
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			try {
+				socket = new Socket(destinationAddress, destinationPort);
+
+				Object Playfield;
+				byte[] buffer = new byte[1024];
+
+				int bytesRead;
+				InputStream inputStream = socket.getInputStream();
+
+				while ((bytesRead = inputStream.read(buffer)) != -1){
+					byteArrayOutputStream.write(buffer, 0, bytesRead);
+					response += byteArrayOutputStream.toString("UTF-8");
+				}
+
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally{
+				if(socket != null){
+					try {
+						socket.close();
+					} catch (IOException e) {e.printStackTrace();}
+				}
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+
+			// TODO :: Send changes during turn
+			gamefield.setField(new Gamefield);
+
+			super.onPostExecute(result);
+		}
+	}
+
+*/ // END OF COMMENT :: NETWORKING
 }
