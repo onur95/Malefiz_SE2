94cadbc0fa6e674678ab8fe2d875c3cdd8958751
onur95 <onur95@gmx.at>
2017-04-10 12:31:27 +0200

    Screenanpassungen durchgeführt.

--- core/src/com/mygdx/malefiz/MyMalefizGame.java
+++ core/src/com/mygdx/malefiz/MyMalefizGame.java
@@ -3,6 +3,7 @@ package com.mygdx.malefiz;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,31 +18,26 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 
-public class MyMalefizGame extends ApplicationAdapter {
+public class MyMalefizGame implements Screen {
+
 	private static Stage stage;
 	Texture txt_playground;
 	Image img_playground;
+	final Malefiz mal;
 
-	@Override
-	public void create () {
+	public MyMalefizGame (final Malefiz mal) {
+		this.mal=mal;
 		stage=new Stage(new FitViewport(1500,1498));
 		Gdx.input.setInputProcessor(stage);
 		txt_playground = new Texture("Playboard.jpg");
 		img_playground=new Image(txt_playground);
 		Board.init();
 		BoardToPlayboard.init();
-		Gdx.graphics.setContinuousRendering(false);
-		Gdx.graphics.requestRendering();
+		//auskommentiert da die Bewegung sonst hängt
+		//Gdx.graphics.setContinuousRendering(false);
+		//Gdx.graphics.requestRendering();
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.addActor(img_playground);
-		stage.act();
-		stage.draw();
-		BoardToPlayboard.generate();
-	}
 
 	public static Stage getState(){
 		return stage;
@@ -49,9 +45,38 @@ public class MyMalefizGame extends ApplicationAdapter {
 
 
 	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.addActor(img_playground);
+		stage.act();
+		stage.draw();
+		BoardToPlayboard.generate();
+	}
+
+	@Override
 	public void resize (int width, int height) {
 		// See below for what true means.
 		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
 	}
 
 	@Override
