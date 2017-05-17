5aa5d0fd33e98396b4882222d1935c646b96769b
Tom <thauer@edu.aau.at>
2017-05-17 13:16:21 +0200

    One minor test

---
+++ android/src/com/mygdx/malefiz/mockJenkinsAndro.java
@@ -0,0 +1,11 @@
+package com.mygdx.malefiz;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class mockJenkinsAndro {
+    @Test
+    public void gibTru(){
+        Assert.assertEquals(1,1);
+    }
+}
---
+++ desktop/src/com/mygdx/malefiz/desktop/mockJenkinsDesk.java
@@ -0,0 +1,11 @@
+package com.mygdx.malefiz.desktop;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class mockJenkinsDesk {
+    @Test
+    public void gibTru(){
+        Assert.assertEquals(1,1);
+    }
+}
