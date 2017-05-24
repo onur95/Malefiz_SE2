9da4283cbd8caad898fa8f2de32e170bfaa6b7d8
LiPl007 <lioba.pleschberger@gmx.at>
2017-05-24 19:33:10 +0200

    new class coordinates & BoardUpdate

--- core/src/com/mygdx/malefiz/BoardUpdate.java
+++ core/src/com/mygdx/malefiz/BoardUpdate.java
@@ -3,10 +3,26 @@ package com.mygdx.malefiz;
 
 // Schnittstelle zu Networking
 
-public class BoardUpdate {
-    public static void movePlayer(int actorIndex, int column, int row){
-        // Fooooo.
+import java.io.Serializable;
+
+public class BoardUpdate implements Serializable{
+    private int actorIndex, column, row;
+
+    public BoardUpdate(int actorIndex, int column, int row) {
+        this.actorIndex = actorIndex;
+        this.column = column;
+        this.row = row;
     }
 
+    public int getActorIndex() {
+        return actorIndex;
+    }
 
+    public int getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
 }
---
+++ core/src/com/mygdx/malefiz/Coordinates.java
@@ -0,0 +1,26 @@
+package com.mygdx.malefiz;
+
+/**
+ * Created by Lilibeth on 22.05.2017.
+ */
+
+public class Coordinates {
+    private float xOffset, yOffset;
+
+    public Coordinates(){
+
+    }
+
+    public Coordinates(float xOffset, float yOffset) {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+    }
+
+    public float getxOffset() {
+        return xOffset;
+    }
+
+    public float getyOffset() {
+        return yOffset;
+    }
+}
